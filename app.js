2aea69eef0c970c14d0537eec9eff7a60c2734ac
quochuybh0404 <70765405+quochuybh0404@users.noreply.github.com>
2023-07-09 15:41:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ const app = {
           // const progressPercent = Math.floor(audio.currentTime/audio.duration *100)
           // progress.value = progressPercent
           progress.value = audio.currentTime/audio.duration *100
-          _this.setConfig('currentTime', progress.value)
+          // _this.setConfig('currentTime', progress.value)
         }
         
       }
@@ -224,7 +224,7 @@ const app = {
             // const progressPercent = Math.floor(audio.currentTime/audio.duration *100)
             // progress.value = progressPercent
             progress.value = audio.currentTime/audio.duration *100
-            _this.setConfig('currentTime', progress.value)
+            // _this.setConfig('currentTime', progress.value)
           }
         }
       }
@@ -300,8 +300,8 @@ const app = {
     loadConfig: function() {
       this.isRandom = this.config.isRandom
       this.isRepeat = this.config.isRepeat
-      this.currentIndex = this.config.musicIndex
-      progress.value = this.config.currentTime
+      // this.currentIndex = this.config.musicIndex
+      // progress.value = this.config.currentTime
       
     },
 
