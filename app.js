a399c0bb84e146182d3ad0d94c86948e6a54861c
quochuybh0404 <70765405+quochuybh0404@users.noreply.github.com>
2023-07-09 15:47:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -300,7 +300,7 @@ const app = {
     loadConfig: function() {
       this.isRandom = this.config.isRandom
       this.isRepeat = this.config.isRepeat
-      this.currentIndex = this.config.musicIndex
+      // this.currentIndex = this.config.musicIndex
       // progress.value = this.config.currentTime
       
     },
@@ -395,7 +395,7 @@ const app = {
         randomBtn.classList.toggle('active', this.isRandom) 
         repeatBtn.classList.toggle('active', this.isRepeat)
 
-        // this.musicPlayed[0] = this.config.musicIndex       
+        this.musicPlayed[0] = this.config.musicIndex       
         
         // this.playFromSavedPosition()
     }
